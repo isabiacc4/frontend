4f31db335344078ca1eaeca2da8e338e90cacedb
Gabrielly <amorim.isadora87@gmail.com>
2024-06-11 11:03:26 -0300

    subindo delete dos json fora da pasta, header e footer

---
+++ cadeprofessor/components/footer.jsx
@@ -0,0 +1,37 @@
+import { Text, image, Statusbar, View, StyleSheet } from 'react-native';
+
+const Footer = () => {
+    return (
+
+        <View style={styles.container}>
+
+            <Image
+                source={require("../assets/logofooter1.png")}
+                style={{ width: 50, height: 50 }}>
+            </Image>
+
+            <View>
+                <Text>Curso Técnico de Desenvolvimento de Sistemas</Text>
+            </View>
+
+            <Image
+                source={require("../assets/logofooter2.png")}
+                style={{ width: 50, height: 50 }}>
+            </Image>
+
+
+
+        </View >
+
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+})
+
+export default Footer;
---
+++ cadeprofessor/components/header.jsx
@@ -0,0 +1,31 @@
+import { Text, image, Statusbar, View, StyleSheet } from 'react-native';
+
+const Header = () => {
+    return(
+
+        <View style = { styles.container } >
+    
+        <Image
+            source={require("../assets/logoheader.png")}
+            style={{ width: 50, height: 50 }}>
+        </Image>
+
+        <View>
+        <Text>Onde está o professor?</Text>
+        </View>
+
+   
+    </View >
+       
+    )}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+})
+
+
+export default Header;
